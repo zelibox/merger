8d5e536e85bb4bbbb20d76715ebc2fa9ad60ab76
maxim <zeli.box@gmail.com>
2018-11-27 00:21:07 +0300

    init

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader", // creates style nodes from JS strings
+                    "css-loader", // translates CSS into CommonJS
+                    "sass-loader" // compiles Sass to CSS, using Node Sass by default
+                ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ]
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+};
